152b7ffd403ac5e83eb43fbd5f2af857658be4cd
felixNyalenda <felixasejunior@gmail.com>
2023-04-22 23:04:22 +0300

    file config

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { Inter, Open_Sans } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+const openSans = Open_Sans({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "aseDesign AI Blog",
+  description: "Built in Next JS and uses AI",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
