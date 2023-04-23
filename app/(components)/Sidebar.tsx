ec78bda0b84e8c25a78e0f4cd46f25288fd283c2
felixNyalenda <felixasejunior@gmail.com>
2023-04-23 05:33:32 +0300

    sidebar section build

--- app/(components)/Sidebar.tsx
+++ app/(components)/Sidebar.tsx
@@ -1,7 +1,44 @@
+import SocialLinks from "./SocialLinks";
+import Subscribe from "./Subscribe";
+
 type Props = {};
 
 const Sidebar = (props: Props) => {
-  return <div>Sidebar</div>;
+  return (
+    <section>
+      <h4 className="bg-wh-900 py-3 px-5 text-wh-50 text-sm text-center font-bold ">
+        Subscribe & Follow
+      </h4>
+
+      <div className="m-5">
+        <SocialLinks isDark />
+      </div>
+
+      <div className="block md:hidden">
+        <Subscribe />
+      </div>
+
+      <div className="bg-wh-900 my-8">advert img</div>
+
+      <h4 className="bg-wh-900 py-3 px-5 text-wh-50 text-sm text-center font-bold ">
+        About the Blog
+      </h4>
+
+      <div className="bg-wh-900 my-8">profile img</div>
+
+      <h4 className="py-3 px-5 text-wh-500 text-center font-bold ">
+        Felix Nyalenda
+      </h4>
+
+      <p className="text-sm text-center text-wh-500 line-clamp-4">
+        Things have been happening in my life since we last spoke. For instance,
+        I just discovered that my plumber is not actually my plumber—the plumber
+        I hired subcontracted another plumber. He is a broker. Do you know you
+        can never be a millionaire by being a broker? No? The clue is in the
+        name, ‘broke-r’.
+      </p>
+    </section>
+  );
 };
 
 export default Sidebar;
--- app/(components)/SocialLinks.tsx
+++ app/(components)/SocialLinks.tsx
@@ -19,7 +19,7 @@ const SocialLinks = ({ isDark = false }: Props) => {
     <div className="flex justify-between items-center gap-7">
       <a href="https://twitter.com/" rel="noreferrer" target="_blank">
         <Image
-          className={`${isDark ? "brightness-0" : ""}hover:opacity-50`}
+          className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
           src={twitter}
           alt="twitter"
           width={20}
@@ -29,7 +29,7 @@ const SocialLinks = ({ isDark = false }: Props) => {
 
       <a href="https://facebook.com/" rel="noreferrer" target="_blank">
         <Image
-          className={`${isDark ? "brightness-0" : ""}hover:opacity-50`}
+          className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
           src={facebook}
           alt="facebook"
           width={20}
@@ -39,7 +39,7 @@ const SocialLinks = ({ isDark = false }: Props) => {
 
       <a href="https://instagram.com/" rel="noreferrer" target="_blank">
         <Image
-          className={`${isDark ? "brightness-0" : ""}hover:opacity-50`}
+          className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
           src={instagram}
           alt="instagram"
           width={20}
@@ -49,7 +49,7 @@ const SocialLinks = ({ isDark = false }: Props) => {
 
       <a href="https://google.com/" rel="noreferrer" target="_blank">
         <Image
-          className={`${isDark ? "brightness-0" : ""}hover:opacity-50`}
+          className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
           src={google}
           alt="google"
           width={20}
@@ -59,7 +59,7 @@ const SocialLinks = ({ isDark = false }: Props) => {
 
       <a href="https://discord.com/" rel="noreferrer" target="_blank">
         <Image
-          className={`${isDark ? "brightness-0" : ""}hover:opacity-50`}
+          className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
           src={discord}
           alt="discord"
           width={20}
@@ -69,7 +69,7 @@ const SocialLinks = ({ isDark = false }: Props) => {
 
       <a href="https://pinterest.com/" rel="noreferrer" target="_blank">
         <Image
-          className={`${isDark ? "brightness-0" : ""}hover:opacity-50`}
+          className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
           src={pinterest}
           alt="pinterest"
           width={20}
